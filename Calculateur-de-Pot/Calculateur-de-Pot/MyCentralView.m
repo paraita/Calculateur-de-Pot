8d9b00398ba028a05f99f31ba9a76742070cc0fa
Mehdi06 <mehdi.mrah@gmail.com>
2012-04-04 11:30:53 +0200

    Nouvelles vues, Background et Cartes

--- Calculateur-de-Pot/Calculateur-de-Pot/MyCentralView.m
+++ Calculateur-de-Pot/Calculateur-de-Pot/MyCentralView.m
@@ -19,13 +19,18 @@
     return self;
 }
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+    UIImageView *background = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"backgroundcartes.jpg"]];
+    [self addSubview:background];
+    NSLog(NSStringFromCGRect(self.bounds));
+    [background release];
+    
 }
-*/
+
 
 @end
